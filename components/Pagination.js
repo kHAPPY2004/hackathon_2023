14dea0e029fd5e23b160b3f32faa74cf9c747206
Happy Kamboj <happykamboj@gmail.com>
2023-10-10 16:56:18 +0530

    updating pagination to showing dynamically pages

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,55 +1,125 @@
+import React from "react";
 import { Link as ScrollLink } from "react-scroll";
 
-const Pagination = ({
-  items,
-  pageSize,
-  currentPage,
-  onPageChange,
-  onPrev,
-  onNext,
-}) => {
+const Pagination = ({ items, pageSize, currentPage, onPageChange }) => {
   const pagesCount = Math.ceil(items / pageSize);
-  if (pagesCount === 1) return null;
-  const pages = Array.from({ length: pagesCount }, (_, i) => i + 1);
 
-  return (
-    <div className="xl:flex xl:space-x-2 mt-3 xl:mt-0 font-medium text-lg cursor-pointer bg-slate-100 dark:bg-slate-600 dark:xl:bg-transparent xl:bg-transparent">
+  if (pagesCount <= 1) return null;
+
+  const visiblePageCount = 3; // Adjust this number to change the number of visible pages in the middle
+  const halfVisiblePages = Math.floor(visiblePageCount / 2);
+
+  let startPage, endPage;
+  if (pagesCount <= visiblePageCount) {
+    startPage = 1;
+    endPage = pagesCount;
+  } else if (currentPage <= halfVisiblePages) {
+    startPage = 1;
+    endPage = visiblePageCount;
+  } else if (currentPage + halfVisiblePages >= pagesCount) {
+    startPage = pagesCount - visiblePageCount + 1;
+    endPage = pagesCount;
+  } else {
+    startPage = currentPage - halfVisiblePages;
+    endPage = currentPage + halfVisiblePages;
+  }
+
+  const pages = [];
+
+  if (currentPage > 1) {
+    pages.push(
       <ScrollLink
         className="hover:text-gray-800 px-2 xl:px-0 dark:hover:text-gray-200 flex justify-center md:justify-start my-2 xl:my-0"
-        onClick={() => onPrev()}
+        onClick={() => onPageChange(currentPage - 1)}
         to="problem_statement1"
         smooth={true}
         duration={500}
+        key="prev"
       >
         Previous
       </ScrollLink>
-      <div className="overflow-auto mx-1 xl:w-full flex justify-start">
-        <div className="flex space-x-2">
-          {pages.map((page) => (
-            <ScrollLink
-              className={`${currentPage === page ? "bg-gray-400 " : ""}${
-                currentPage === page && "dark:bg-blue-300"
-              }border px-2 rounded-md hover:bg-gray-300 border-black dark:border-white dark:hover:bg-gray-600`}
-              onClick={() => onPageChange(page)}
-              to="problem_statement1"
-              smooth={true}
-              duration={500}
-              key={page}
-            >
-              {page}
-            </ScrollLink>
-          ))}
-        </div>
-      </div>
+    );
+  }
+
+  if (startPage > 1) {
+    pages.push(
       <ScrollLink
-        className="hover:text-gray-800 px-2 xl:px-0 dark:hover:text-gray-200 flex justify-center md:justify-start my-2 xl:my-0"
-        onClick={() => onNext()}
+        className="border px-2 rounded-md hover:bg-gray-300 border-black dark:border-white dark:hover:bg-gray-600"
+        onClick={() => onPageChange(1)}
         to="problem_statement1"
         smooth={true}
         duration={500}
+        key={1}
+      >
+        1
+      </ScrollLink>
+    );
+    if (startPage > 2) {
+      pages.push(
+        <span className="text-gray-400" key="start-ellipsis">
+          ...
+        </span>
+      );
+    }
+  }
+
+  for (let page = startPage; page <= endPage; page++) {
+    pages.push(
+      <ScrollLink
+        className={`${currentPage === page ? "bg-gray-400 " : ""}${
+          currentPage === page && "dark:bg-blue-300"
+        }border px-2 rounded-md hover:bg-gray-300 border-black dark:border-white dark:hover:bg-gray-600`}
+        onClick={() => onPageChange(page)}
+        to="problem_statement1"
+        smooth={true}
+        duration={500}
+        key={page}
+      >
+        {page}
+      </ScrollLink>
+    );
+  }
+
+  if (endPage < pagesCount) {
+    if (endPage < pagesCount - 1) {
+      pages.push(
+        <span className="text-gray-400" key="end-ellipsis">
+          ...
+        </span>
+      );
+    }
+    pages.push(
+      <ScrollLink
+        className="border px-2 rounded-md hover:bg-gray-300 border-black dark:border-white dark:hover:bg-gray-600"
+        onClick={() => onPageChange(pagesCount)}
+        to="problem_statement1"
+        smooth={true}
+        duration={500}
+        key={pagesCount}
+      >
+        {pagesCount}
+      </ScrollLink>
+    );
+  }
+
+  if (currentPage < pagesCount) {
+    pages.push(
+      <ScrollLink
+        className="hover:text-gray-800 px-2 xl:px-0 dark:hover:text-gray-200 flex justify-center md:justify-start my-2 xl:my-0"
+        onClick={() => onPageChange(currentPage + 1)}
+        to="problem_statement1"
+        smooth={true}
+        duration={500}
+        key="next"
       >
         Next
       </ScrollLink>
+    );
+  }
+
+  return (
+    <div className="select-none pagination-container xl:flex xl:space-x-2 mt-3 xl:mt-0 font-medium text-lg cursor-pointer bg-slate-100 dark:bg-slate-600 dark:xl:bg-transparent xl:bg-transparent">
+      {pages}
     </div>
   );
 };
